9558df94bd818b82dff0936df261beb7dc6b3549
LOKI2522 <wlogesh@gmail.com>
2026-07-20 16:18:37 +0530

    created files

---
+++ mobile/lib/app/routes/app_routes.dart
@@ -0,0 +1,9 @@
+class AppRoutes {
+  static const splash = "/";
+  static const home = "/home";
+  static const createRoom = "/create-room";
+  static const joinRoom = "/join-room";
+  static const transfer = "/transfer";
+  static const history = "/history";
+  static const settings = "/settings";
+}
